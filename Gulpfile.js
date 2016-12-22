e9e6b0f64fd14d8401d61b51fe1c8f6512f37a12
Jimmy Zooler <jimmy.zooler.com>
2016-12-22 14:21:17 -0500

    Added package.json, gulpfile.js, and gitignore, installed gulp.

---
+++ Gulpfile.js
@@ -0,0 +1,82 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+    
+//Styles
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cssnano())
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// Scripts
+//gulp.task('scripts', function() {
+//  return gulp.src('js/**/*.js')
+//    .pipe(jshint('.jshintrc'))
+//    .pipe(jshint.reporter('default'))
+//    .pipe(concat('main.js'))
+//    .pipe(gulp.dest('dist/js'))
+//    .pipe(rename({suffix: '.min'}))
+//    .pipe(uglify())
+//    .pipe(gulp.dest('dist/js'))
+//    .pipe(notify({ message: 'Scripts task complete' }));
+//});
+
+//Compress Images
+gulp.task('images', function() {
+  return gulp.src('images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+// Clean
+gulp.task('clean', function() {
+  return del(['dist/styles', 'dist/images']);
+});
+
+// Default Task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'images');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('sass/**/*.scss', ['styles']);
+
+  // Watch .js files
+  //gulp.watch('js/**/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('images/**/*', ['images']);
+
+  // Create LiveReload server
+  livereload.listen();
+
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+
+});
