e85ef78c6444d0cb038c436c54eb073f7e0e57e9
Jimmy Zooler <jimmy.zooler.com>
2016-12-23 23:23:19 -0500

    Began devlopment

---
+++ template-home.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: Home Page
+*/
+?>
+
+<?php get_header(); ?>
+			
+<div id="main">
+    <div class="main-intro-page">
+        <div class="intro-content">
+            
+        </div>
+    </div>
+    <div class="main-view" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/brian_center.jpg');">
+        <div class="pattern-overlay" style="background: url('<?php echo get_template_directory_uri(); ?>/images/pattern_overlay.png') repeat;"></div>
+    </div>
+    <div class="dark-overlay"></div>
+    <div class="social-buttons">
+        
+    </div>
+    <div class="next-triangle"><img src="<?php echo get_template_directory_uri(); ?>/images/triangle-icon2.png" /></div>
+    <div class="prev-triangle"><img src="<?php echo get_template_directory_uri(); ?>/images/triangle-icon2.png" /></div>
+</div>
+
+<?php get_footer(); ?>
