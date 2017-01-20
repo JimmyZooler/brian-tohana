d76f5c2b7bda06a4e71755f454ee62f2517527a1
Jimmy Zooler <jimmy.zooler.com>
2017-01-20 13:49:24 -0500

    Added reveal locally to test homepage slides.

---
+++ template-about.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: About Page
+*/
+?>
+
+<?php get_header(); ?>
+			
+<div id="main">
+    <div class="main-view" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/brian_center.jpg');">
+        <div class="pattern-overlay" style="background: url('<?php echo get_template_directory_uri(); ?>/images/black-squares.png') repeat;"></div>
+        <div class="dark-overlay"></div>
+    </div>
+    <div class='open-footer'><span class='arrow'></span><span>CONNECT</span></div>
+</div>
+
+<?php get_footer(); ?>
--- template-home.php
+++ template-home.php
@@ -8,17 +8,18 @@ Template Name: Home Page
 			
 <div id="main">
     <div class="main-view" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/brian_center.jpg');">
-        <div class="pattern-overlay" style="background: url('<?php echo get_template_directory_uri(); ?>/images/black-squares.png') repeat;"></div>
-        <div class="dark-overlay"></div>
+<!--        <div class="pattern-overlay" style="background: url('<?php echo get_template_directory_uri(); ?>/images/black-squares.png') repeat;"></div>
+        <div class="dark-overlay"></div>-->
+        <iframe width="100%" height="100%" style="position: absolute; border: none;" marginheight="0" marginwidth="0" src="<?php echo get_template_directory_uri(); ?>/reveal.js-3.4.0/index.html#/">
+            Sorry your browser does not support this playfulness!
+          </iframe>
     </div>
-    <div class="social-buttons">
-    </div>
-    <div class="next-triangle"><img src="<?php echo get_template_directory_uri(); ?>/images/triangle-icon2_larger.png" /></div>
-    <div class="prev-triangle"><img src="<?php echo get_template_directory_uri(); ?>/images/triangle-icon2_larger.png" /></div>
+<!--    <div class="next-triangle"><img src="<?php echo get_template_directory_uri(); ?>/images/triangle-icon2_larger.png" /></div>
+    <div class="prev-triangle"><img src="<?php echo get_template_directory_uri(); ?>/images/triangle-icon2_larger.png" /></div>-->
     <div class="main-intro">
         <div class="intro-content">
             <div class="title-box">
-                <a class="btn enter">Enter<span class="arrow"></span></a>
+                <a class="btn enter">I Believe<span class="arrow"></span></a>
             </div>
         </div>
         <div class="dark-overlay"></div>
