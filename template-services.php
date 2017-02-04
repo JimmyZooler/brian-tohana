9232426bf510550a43ba02a7c02d59ca102752a0
Jimmy Zooler <jimmy.zooler.com>
2017-02-04 16:04:53 -0500

    Added markup and styles and content for services page.

--- template-services.php
+++ template-services.php
@@ -5,32 +5,58 @@ Template Name: Services Page
 ?>
 
 <?php get_header(); ?>
-			
-<div id="main-pages">
-    <div class="main-view-pages">
-        <div class="inner-content">
-            <h1>Services</h1>
-            <hr>
-            <div class="services-section">
-                <h2>Coaching</h2>
-                <p>Coaching is like leadership. As I guide you into yourself I call forward your highest truth and potential to allow you to become more than you realize you can be.</p>
-                
-                <div class="services">
-                    
-                </div>
-                <div class="services">
-                    
-                </div>
-                <div class="services">
-                    
-                </div>
-            </div>
-            <div class="services-section">
-                <h2>Facilitation</h2>
-                <div class="services">
-                    
-                </div>
-            </div>
+<header class="page-header" style="background-image: url('<?php echo get_template_directory_uri(); ?>/dist/images/brian_parlament.jpg');">
+    <div class="helper">
+        <h1>Services</h1>
+        <h3>Coaching is like leadership. As I guide you into yourself I call forward your highest truth and potential to allow you to become more than you realize you can be.</h3>
+    </div>
+    <div class="dark-overlay"></div>
+</header>
+<div class="coaching">
+    <div class="page-section">
+        <h2>Coaching</h2>
+        <h3>My philosophy:</h3>
+        <p>You don’t need fixing because there’s nothing wrong with you.</p>
+            <p>You don’t need to be told what to do because the answers are inside of you.</p>
+        <div class="services">
+            <img src='<?php echo get_template_directory_uri(); ?>/dist/images/intimacy.png' />
+            <h3>Relationships + Intimacy</h3>
+            <p>We compromise our truth for company.</p>
+            <p>We’re not alone, but we feel alone.</p>
+            <p>We want to be accepted for who we are but we’re afraid to be seen as we are.</p>
+        </div>
+        <div class="services">
+            <img src='<?php echo get_template_directory_uri(); ?>/dist/images/business.png' />
+            <h3>Personal Development + Business</h3>
+            <p>What got you here won’t get you there.</p>
+            <p>What we’re most scared of we’re also most passionate about.</p>
+            <p>We’re afraid of what we might find when we really look at ourselves.</p>
+        </div>
+        <div class="services">
+            <img src='<?php echo get_template_directory_uri(); ?>/dist/images/healing.png' />
+            <h3>Transformation + Healing</h3>
+            <p>We’re all hurting but we hide our pain.</p>
+            <p>We don’t feel worthy of what we want.</p>
+            <p>We want to be free from limitation to be our true self.</p>
+        </div>
+    </div>
+</div>
+<div class="facilitation">
+    <div class="page-section">
+        <h2>Facilitation</h2>
+        <div class="services">
+            <img src='<?php echo get_template_directory_uri(); ?>/dist/images/circling_1.png' />
+            <h3>Authentic Relating + Circling</h3>
+            <p>Allow a visceral experience of connection and understanding of another person’s world.</p>
+            <p>Break through assumptions and projections we have about each other.</p>
+            <p>Enter present moment awareness to be at peace with what is.</p>
+        </div>
+        <div class="services">
+            <img src='<?php echo get_template_directory_uri(); ?>/dist/images/infinity_1.png' />
+            <h3>CreativeHeart Group (Mastermind with a twist!)</h3>
+                <p>We don’t have to be strong to belong.</p>
+                <p>It’s less about what we do and more about who we are.</p>
+                <p>We allow our heart’s calling to lead the way.</p>
         </div>
     </div>
 </div>
