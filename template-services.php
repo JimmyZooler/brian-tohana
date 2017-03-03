972ee51e5e131a288689d7b87f49ea098444b45f
Jimmy Zooler <jimmy.zooler.com>
2017-03-02 22:56:19 -0500

    Added all slides and did header edits.

--- template-services.php
+++ template-services.php
@@ -17,31 +17,10 @@ Template Name: Services Page
     <div class="page-section">
         <h2>Coaching</h2>
         <?php the_field('my_philosophy'); ?>
-        <?php the_field('section_2'); ?>
+        <div id="4-pillars"><?php the_field('section_2'); ?></div>
         <?php the_field('section_3'); ?>
         <?php the_field('typical_session'); ?>
         
-<!--        <div class="services">
-            <img src='<?php echo get_template_directory_uri(); ?>/dist/images/intimacy.png' />
-            <h3>Relationships + Intimacy</h3>
-            <p>We compromise our truth for company.</p>
-            <p>We’re not alone, but we feel alone.</p>
-            <p>We want to be accepted for who we are but we’re afraid to be seen as we are.</p>
-        </div>
-        <div class="services">
-            <img src='<?php echo get_template_directory_uri(); ?>/dist/images/business.png' />
-            <h3>Personal Development + Business</h3>
-            <p>What got you here won’t get you there.</p>
-            <p>What we’re most scared of we’re also most passionate about.</p>
-            <p>We’re afraid of what we might find when we really look at ourselves.</p>
-        </div>
-        <div class="services">
-            <img src='<?php echo get_template_directory_uri(); ?>/dist/images/healing.png' />
-            <h3>Transformation + Healing</h3>
-            <p>We’re all hurting but we hide our pain.</p>
-            <p>We don’t feel worthy of what we want.</p>
-            <p>We want to be free from limitation to be our true self.</p>
-        </div>-->
     </div>
 </div>
 <div class="facilitation">
